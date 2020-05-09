a0c2985baf7f7abb38c34a17b7dbf7eec10ddb57
Jake Kile <jkile9@gmail.com>
2020-05-08 18:43:51 -0700

    updated about on home:

--- components/About/About.js
+++ components/About/About.js
@@ -4,12 +4,9 @@ export default function About() {
     return (
         <div className="box has-background-dark">
             <div className="content is-medium">
-            <p className="is-medium has-text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Ad, quos mollitia nesciunt repellat molestiae dolore iste pariatur tempore ducimus voluptatem maiores et facilis, natus temporibus? 
-            Fugit, magni excepturi asperiores dicta assumenda blanditiis, veniam in vel minima animi illo quaerat repellendus esse. 
-            Veritatis illum quod dolorem quibusdam itaque, placeat ducimus, dolore quis quisquam nesciunt temporibus fuga dolores unde excepturi exercitationem? 
-            Voluptas sequi animi earum suscipit, voluptatum officiis quis natus dolorum quasi molestias blanditiis ratione corrupti id eius doloribus recusandae commodi nisi? Officia dolore molestias cupiditate perferendis accusamus, aperiam nesciunt. 
-            Officiis labore amet reiciendis error sequi dicta debitis repellendus, ipsum quaerat voluptate.</p>
+            <p className="is-medium has-text-white">The future of television. Scripted and improvised talk shows, radio, how to, cooking, and more. Handmade by real life human beings, every week. 
+
+Its DIY public access tv, live from James World.</p>
             </div>
         </div>
     )
